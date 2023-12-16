579951d5eaea0c3ae5a6e068f6744085ad969f09
thirtyandthree <134704269+thirtyandthree@users.noreply.github.com>
2023-12-16 11:53:21 +0800

    Update GeminiBotConfig.py

--- GeminiBotConfig.py
+++ GeminiBotConfig.py
@@ -1,6 +1,6 @@
 ﻿#Create at https://makersuite.google.com/
-GOOGLE_AI_KEY = "GOOGLE_AI_KEY"
+GOOGLE_AI_KEY = "AIzaSyAGo0iTX2r4699UbTGUVqPb51O9f2q2XUY"
 #Create at https://discord.com/developers/applications
-DISCORD_BOT_TOKEN = "DISCORD_BOT_KEY"
+DISCORD_BOT_TOKEN = "MTE4NTQyMDE5OTExODI2MjM1Mg.G48bKp.uSnxVtGuaWHrKcltUogtR8EqEHKUf_hId2JQZs"
 #Disable history by setting to 0
 MAX_HISTORY = 15
